40bee0afb46ad2ce6a10068a9078996b549722f2
yinfeiru <yinfeiru@gmail.com>
2015-08-23 17:45:56 -0700

    add db schema

---
+++ hireio/tools/schema/08_23_2015_create_account_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE account (
+                      first_name VARCHAR(20),
+                      last_name VARCHAR(20),
+                      account_id VARCHAR(50),
+                      password VARCHAR(50)
+                     );
