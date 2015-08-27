7997e99d39990b17e4bd3b1d39a5672a0ad50672
yinfeiru <yinfeiru@gmail.com>
2015-08-26 19:56:22 -0700

    registration is working

--- hireio/recruiters/models.py
+++ hireio/recruiters/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class AccountInfo(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    account_id = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     is_engineer = models.BooleanField(default=False)
--- hireio/recruiters/views.py
+++ hireio/recruiters/views.py
@@ -2,9 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext, loader
 
+from recruiters.models import AccountInfo
+
+from recruiters import forms
+
 def register(request):
     if request.method == 'GET':
-        template = loader.get_template('recruiters/register.html')
-        return HttpResponse(template.render())
+        return show_registration_page(request)
     elif request.method == 'POST':
-        None
+        form = forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            AccountInfo.objects.create(
+                user_name = form.data.get('username'),
+                email = form.data.get('email'),
+# TODO need to encrypt it
+# TODO need to use https
+                password = form.data.get('password'),
+            )
+            return render(
+                request,
+                'landing_pages/index.html',
+            )
+        else:
+            return show_registration_page(request)
+
+def show_registration_page(request):
+    template = 'recruiters/register.html'
+    form = forms.RegistrationForm()
+    return render(
+        request,
+        template,
+        {
+            'form': form,
+        },
+    )
+
