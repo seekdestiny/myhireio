aba26fa8cb7ac5ecba0774e80e507dc0806c172d
yinfeiru <yinfeiru@gmail.com>
2015-09-09 07:38:30 -0700

    the majority of backend functionalities are done, not well polished tho

--- hireio/candidates/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class AccountInfo(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    account_id = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
--- hireio/recruiters/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import RequestContext, loader
-
-from recruiters.models import AccountInfo
-
-
