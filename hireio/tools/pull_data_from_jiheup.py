b8452fbaac1ae6585157b1f73551e47fcb8dad31
yinfeiru <yinfeiru@gmail.com>
2015-09-26 09:41:14 -0700

    able to get data from jiheup

--- hireio/tools/pull_data_from_jiheup.py
+++ hireio/tools/pull_data_from_jiheup.py
@@ -10,18 +10,20 @@ auth_response = session.post(
         'client_id': 'eE0MRGsAnNsjy0ZQ83Kz8S6xM1oYIGcW3RBwJdbcIBgAUaIe6jnSc+h41j8dc1LX.dist',
         'client_secret': 'xqUAQbAc/H0OiqVMEYSne+2QxabFUGRW2l94lAg0hujaCb5Kx40YYqzeRa8jKSr6.dist',
         'grant_type': 'client_credentials',
+        'scope': 'board.read'
     },
 )
 
-auth_json = json.load(auth_response.text)
-import pdb;pdb.set_trace()
-#companies_reponse = session.post(
-#    url='https://api.jiheup.com/v1/companies',
-#    data={
-#        'access_token': auth_response.text["access_token"],
-#        'page': 1,
-#        'per_page': 10,
-#    }
-#)
-#
-#print companies_response.text
+auth_json = json.loads(auth_response.text)
+companies_reponse = session.get(
+    url='http://api-qa.jihub.com:80/v1/positions',
+    data={
+        'access_token': auth_json['access_token'],
+        'page': 1,
+        'per_page': 10,
+    }
+)
+
+print companies_reponse.text
+
+
