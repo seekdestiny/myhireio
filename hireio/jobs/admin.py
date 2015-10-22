b43cdf08d55fdd7568a2cb4f433d7790782990bd
seekdestiny <jifeiqian@gmail.com>
2015-10-21 21:18:23 -0700

    make list format better

--- hireio/jobs/admin.py
+++ hireio/jobs/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
 from .models import Company
-# Register your models here.
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'tag_name', 'product', 'why_us', 'traction', 'funding', 'size',
-                     'market', 'company_link', 'last_active')
+    list_display = (    
+                          'name', 
+                          'tag_name', 
+                          'product', 
+                          'why_us', 
+                          'traction', 
+                          'funding', 
+                          'size',
+                          'market', 
+                          'company_link', 
+                          'last_active'
+                   )
+
     list_filter = ['last_active']
     search_fields = ['name']
 
